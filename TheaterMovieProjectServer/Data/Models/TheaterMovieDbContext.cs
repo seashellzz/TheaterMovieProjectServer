4aca2043495f8d64d8f4ab84d201a88d38acf542
seashellzz <xzzhannah@gmail.com>
2024-05-21 15:55:12 -0700

    minor changes

--- TheaterMovieProjectServer/Data/Models/TheaterMovieDbContext.cs
+++ TheaterMovieProjectServer/Data/Models/TheaterMovieDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using System.Diagnostics.Metrics;
 using System.Security.Cryptography.X509Certificates;
 
 namespace TheaterMovieProjectServer.Data.Models
@@ -14,9 +15,39 @@ namespace TheaterMovieProjectServer.Data.Models
         public TheaterMovieDbContext(DbContextOptions options): base(options) 
         { 
         }
-        public DbSet<Movie> Movies => Set <Movie>();
-        public DbSet<Theater> Theaters => Set <Theater>();
+        public DbSet<Movie> Movies { get; set; } = null!;
+        public DbSet<Theater> Theaters { get; set; } = null!;
 
-       
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured)
+            {
+                return;
+            }
+
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false);
+            IConfigurationRoot configuration = builder.Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Movie>(entity =>
+            {
+                entity.HasOne(d => d.Theater).WithMany(p => p.Movies)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Movie_Theater");
+            });
+
+            builder.Entity<Theater>(entity =>
+            {
+                entity.Property(e => e.Name).IsFixedLength();
+                entity.Property(e => e.Location).IsFixedLength();
+            });
+        }
+
+
     }
 }
